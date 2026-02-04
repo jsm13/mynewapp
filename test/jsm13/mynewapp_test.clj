04ee9242e89c08ca89e6e9cdf81145e10b24c21b
jsm <me@jsm13.com>
2026-02-04 13:05:10 -0500

    Generate deps-new app and git init

---
+++ test/jsm13/mynewapp_test.clj
@@ -0,0 +1,7 @@
+(ns jsm13.mynewapp-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [jsm13.mynewapp :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
