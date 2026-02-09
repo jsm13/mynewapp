58ae3a8dc0aa6a92750d2aedd1cdf8eef4ee2d57
jsm <me@jsm13.com>
2026-02-09 13:23:44 -0500

    Fix test

--- test/jsm13/mynewapp_test.clj
+++ test/jsm13/mynewapp_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer [deftest is testing]]
             [jsm13.mynewapp :as sut])) ; system under test
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest demo-test
+  (testing "Replace me, I don't test anything."
+    (is (= 1 1))))
