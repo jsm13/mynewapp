810bce83482c742735eafe6b04aa1ed120fb0958
jsm <me@jsm13.com>
2026-04-28 10:52:41 -0400

    Update build script to include commit hash

--- build.clj
+++ build.clj
@@ -1,20 +1,28 @@
 (ns build
   (:refer-clojure :exclude [test])
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [clojure.java.shell :as sh]))
 
 (def lib 'net.clojars.jsm13/mynewapp)
-(def version "0.1.0-SNAPSHOT")
+(def base-version "0.0.1")
 (def main 'jsm13.mynewapp)
 (def class-dir "target/classes")
 
+(defn- git-commit-hash []
+  (-> (sh/sh "git" "rev-parse" "--short" "HEAD")
+      :out
+      (clojure.string/trim)))
+
 (defn- uber-opts [opts]
+  (let [git-hash (git-commit-hash)
+        version (str base-version "-" git-hash)]
   (assoc opts
          :lib lib :main main
          :uber-file (format "target/%s-%s.jar" lib version)
          :basis (b/create-basis {})
          :class-dir class-dir
          :src-dirs ["src"]
-         :ns-compile [main]))
+         :ns-compile [main])))
 
 (defn uber "Build the uberjar." [opts]
   (b/delete {:path "target"})
